99513f59eeb43862fb3373bfd3f2f596e8d461cd
khkr <fsemicolon@gmail.com>
2017-06-19 17:10:47 +0530

    binary search tree v1.0

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -1,56 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-  
+
 struct node
 {
-    int key;
-    struct node *left, *right;
+	int key;
+	struct node *left, *right;
 };
-  
+
 struct node *newNode(int item)
 {
-    struct node *temp =  (struct node *)malloc(sizeof(struct node));
-    temp->key = item;
-    temp->left = temp->right = NULL;
-    return temp;
+	struct node *temp =  (struct node *)malloc(sizeof(struct node));
+	temp->key = item;
+	temp->left = temp->right = NULL;
+	return temp;
 }
-  
+
 void inorder(struct node *root)
 {
-    if (root != NULL)
-    {
-        inorder(root->left);
-        printf("%d \t", root->key);
-        inorder(root->right);
-    }
-    }
-  
+	if (root != NULL)
+	{
+		inorder(root->left);
+		printf("%d \t", root->key);
+		inorder(root->right);
+	}
+}
+
 struct node* insert(struct node* node, int key)
 {
     /* If the tree is empty, return a new node */
-    if (node == NULL) return newNode(key);
- 
+	if (node == NULL) return newNode(key);
+
     /* Otherwise, recur down the tree */
-    if (key < node->key)
-        node->left  = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);   
- 
+	if (key < node->key)
+		node->left  = insert(node->left, key);
+	else if (key > node->key)
+		node->right = insert(node->right, key);   
+
     /* return the (unchanged) node pointer */
-    return node;
+	return node;
 }
-  
+
+// void delete(struct node* root,int key)
+// {
+// 	if(root!=NULL)
+// 	{
+// 		if(root->data==key)
+// 		{
+
+// 		}
+		
+// 	}
+	
+
+// }
+
+
 int main()
 {
 	struct node *root = NULL;
 	
 	int choice =1, key;
-
-	printf("Insert a number\n");
-
-	scanf("%d",&key);
-
-	root = insert(root, key);
 
 
 	do
@@ -63,7 +72,10 @@ int main()
 		{
 			case 1: printf("Enter a number\n");
 			scanf("%d",&key); 
-			insert(root,key);
+			if(root==NULL)
+				root = insert(root, key);
+			else
+				insert(root,key);
 			break;
 
 			case 2: inorder(root);
